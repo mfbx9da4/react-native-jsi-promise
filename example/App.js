75d66aa6d7c056102351ce57d296f0be875a7359
David Alberto Adler <dalberto.adler@gmail.com>
2022-01-26 16:31:14 +0000

    update example

--- example/App.js
+++ example/App.js
@@ -3,29 +3,104 @@ import * as React from 'react';
 import {StyleSheet, Text, TouchableOpacity} from 'react-native';
 import jsiPromise from 'react-native-jsi-promise';
 
-export default function App() {
-  const [result, setResult] = React.useState('initial');
+/** Intercepts writes to any property of an object */
+export function observeProperty(obj, property, onChanged, customDescriptor) {
+  let val = obj[property];
+  Object.defineProperty(
+    obj,
+    property,
+    Object.assign(
+      {
+        get() {
+          return val;
+        },
+        set(newVal) {
+          val = newVal;
+          onChanged(newVal);
+        },
+        enumerable: true,
+        configurable: true,
+      },
+      customDescriptor,
+    ),
+  );
+}
+
+function FastExample() {
+  const [state, setState] = React.useState({took: 0, result: 'initial'});
 
   return (
     <TouchableOpacity
-      style={styles.container}
-      onPress={async () => {
+      style={styles.fast}
+      onPress={() => {
         const start = Date.now();
-        const res = await jsiPromise.foo();
-        setResult(`${res} (${Date.now() - start}ms)`);
+        const res = jsiPromise.pointerToReturnedValue();
+        observeProperty(res, 'result', newVal => {
+          setState({took: Date.now() - start, result: {...res}});
+        });
       }}>
-      <Text>Result: {result}</Text>
+      <Text>
+        {'Tap me, will always be fast: \n'}
+        {JSON.stringify(state.result)} {state.took}
+      </Text>
     </TouchableOpacity>
   );
 }
 
+function SlowExample() {
+  const [state, setState] = React.useState({took: 0, result: 'initial'});
+
+  return (
+    <TouchableOpacity
+      style={styles.slow}
+      onPress={async () => {
+        const start = Date.now();
+        const res = await new Promise(resolve => {
+          const r = jsiPromise.pointerToReturnedValue();
+          observeProperty(r, 'result', newVal => {
+            resolve(r);
+          });
+        });
+        setState({took: Date.now() - start, result: {...res}});
+      }}>
+      <Text>
+        {'Tap me, will sometimes be slow: \n'}
+        {JSON.stringify(state.result)} {state.took}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <FastExample />
+      <SlowExample />
+      {/* <TouchableOpacity
+        style={{backgroundColor: 'crimson', flex: 1}}
+        onPress={async () => {
+          forceRender();
+        }}>
+        <Text>Force render</Text>
+      </TouchableOpacity> */}
+    </>
+  );
+}
+
 const styles = StyleSheet.create({
-  container: {
+  fast: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
     backgroundColor: 'gold',
+  },
+  slow: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: 'crimson',
   },
   box: {
     width: 60,
--- ios/JsiPromise.mm
+++ ios/JsiPromise.mm
@@ -36,27 +36,22 @@ static void install(jsi::Runtime &jsiRuntime, JsiPromise *cryptoPp) {
 
     auto myqueue = dispatch_queue_create("myqueue", DISPATCH_QUEUE_CONCURRENT);
 
-    auto foo = jsi::Function::createFromHostFunction(
+    auto promiseResolver = jsi::Function::createFromHostFunction(
         jsiRuntime,
-        jsi::PropNameID::forAscii(jsiRuntime, "foo"),
+        jsi::PropNameID::forAscii(jsiRuntime, "promiseResolver"),
         3,
         [cryptoPp, myqueue](jsi::Runtime& runtime, const jsi::Value& thisValue, const jsi::Value* arguments, size_t count) -> jsi::Value {
 
             CFTimeInterval startTime = CACurrentMediaTime();
 
+            std::shared_ptr<jsi::Value> resolverValue;
+
             auto promiseConstructor = jsi::Function::createFromHostFunction(
                 runtime,
                 jsi::PropNameID::forAscii(runtime, "promiseConstructor"),
                 2,
-                [cryptoPp, startTime, myqueue](jsi::Runtime& runtime, const jsi::Value& thisValue, const jsi::Value* arguments, size_t count) -> jsi::Value {
-                    auto resolverValue = std::make_shared<jsi::Value>((arguments[0].asObject(runtime)));
-
-                    NSLog(@"start");
-                    dispatch_async(myqueue, ^(void){
-                        jsi::Value decryptedJsValue = jsi::Value(runtime, jsi::String::createFromUtf8(runtime, "asdf"));
-                        resolverValue->asObject(runtime).asFunction(runtime).call(runtime, decryptedJsValue);
-                        NSLog(@"took in seconds %f", CACurrentMediaTime() - startTime);
-                    });
+                [&resolverValue, cryptoPp, startTime, myqueue](jsi::Runtime& runtime, const jsi::Value& thisValue, const jsi::Value* arguments, size_t count) -> jsi::Value {
+                    resolverValue = std::make_shared<jsi::Value>(arguments[0].asObject(runtime));
                     return jsi::Value::undefined();
                 }
             );
@@ -67,10 +62,94 @@ static void install(jsi::Runtime &jsiRuntime, JsiPromise *cryptoPp) {
                 .asFunction(runtime)
                 .callAsConstructor(runtime, promiseConstructor);
 
+            dispatch_async(myqueue, ^(void){
+                jsi::Value decryptedJsValue = jsi::Value(runtime, jsi::String::createFromUtf8(runtime, "asdf"));
+                resolverValue->asObject(runtime).asFunction(runtime).call(runtime, decryptedJsValue);
+                NSLog(@"took in seconds %f", CACurrentMediaTime() - startTime);
+            });
+
             return promise;
         }
     );
-    jsiRuntime.global().setProperty(jsiRuntime, "foo", std::move(foo));
+    jsiRuntime.global().setProperty(jsiRuntime, "promiseResolver", std::move(promiseResolver));
+
+    auto pointerToPromiseResolver = jsi::Function::createFromHostFunction(
+        jsiRuntime,
+        jsi::PropNameID::forAscii(jsiRuntime, "pointerToPromiseResolver"),
+        3,
+        [cryptoPp, myqueue](jsi::Runtime& runtime, const jsi::Value& thisValue, const jsi::Value* arguments, size_t count) -> jsi::Value {
+
+            CFTimeInterval startTime = CACurrentMediaTime();
+
+            std::shared_ptr<jsi::Value> resolverValue;
+
+            auto promiseConstructor = jsi::Function::createFromHostFunction(
+                runtime,
+                jsi::PropNameID::forAscii(runtime, "promiseConstructor"),
+                2,
+                [&resolverValue, cryptoPp, startTime, myqueue](jsi::Runtime& runtime, const jsi::Value& thisValue, const jsi::Value* arguments, size_t count) -> jsi::Value {
+                    resolverValue = std::make_shared<jsi::Value>(arguments[0].asObject(runtime));
+                    return jsi::Value::undefined();
+                }
+            );
+
+            auto newPromise = runtime.global().getProperty(runtime, "Promise");
+            auto promise = newPromise
+                .asObject(runtime)
+                .asFunction(runtime)
+                .callAsConstructor(runtime, promiseConstructor);
+
+            dispatch_async(myqueue, ^(void){
+                jsi::Value decryptedJsValue = jsi::Value(runtime, jsi::String::createFromUtf8(runtime, "asdf"));
+                resolverValue->asObject(runtime).asFunction(runtime).call(runtime, decryptedJsValue);
+                NSLog(@"took in seconds %f", CACurrentMediaTime() - startTime);
+            });
+
+            return promise;
+        }
+    );
+    jsiRuntime.global().setProperty(jsiRuntime, "pointerToPromiseResolver", std::move(pointerToPromiseResolver));
+
+    auto pointerToReturnedValue = jsi::Function::createFromHostFunction(
+        jsiRuntime,
+        jsi::PropNameID::forAscii(jsiRuntime, "pointerToReturnedValue"),
+        1,
+        [cryptoPp, myqueue](jsi::Runtime& runtime, const jsi::Value& thisValue, const jsi::Value* arguments, size_t count) -> jsi::Value {
+
+            auto myObj = jsi::Object(runtime);
+            jsi::Value obj = jsi::Value(runtime, myObj);
+            auto myObjRef = std::make_shared<jsi::Value>(obj.asObject(runtime));
+
+
+            dispatch_async(myqueue, ^(void){
+                auto myVal = jsi::Value(runtime, jsi::String::createFromUtf8(runtime, std::to_string(std::rand())));
+                myObjRef->asObject(runtime).setProperty(runtime, "result", myVal);
+            });
+
+            return obj;
+        }
+    );
+    jsiRuntime.global().setProperty(jsiRuntime, "pointerToReturnedValue", std::move(pointerToReturnedValue));
+
+
+//    auto callback = jsi::Function::createFromHostFunction(
+//        jsiRuntime,
+//        jsi::PropNameID::forAscii(jsiRuntime, "callback"),
+//        1,
+//        [cryptoPp, myqueue](jsi::Runtime& runtime, const jsi::Value& thisValue, const jsi::Value* arguments, size_t count) -> jsi::Value {
+//
+//            auto userCallback = arguments[0].getObject(runtime);
+//            auto userCallbackRef = std::make_shared<jsi::Object>(userCallback);
+//
+//            dispatch_async(myqueue, ^(void){
+//                auto myVal = jsi::Value(runtime, jsi::String::createFromUtf8(runtime, std::to_string(std::rand())));
+//                userCallbackRef->asFunction(runtime).call(runtime, myVal);
+//            });
+//
+//            return jsi::Value::undefined();
+//        }
+//    );
+//    jsiRuntime.global().setProperty(jsiRuntime, "callback", std::move(pointerToReturnedValue));
 }
 
 
